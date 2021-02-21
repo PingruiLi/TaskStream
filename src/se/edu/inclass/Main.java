bf2895b1b86b2e0524b8985995bf1040b3305a4d
PingruiLi <lipingrui2001@gmail.com>
2021-02-21 20:34:44 +0800

    add method with lambda

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -6,6 +6,7 @@ import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -20,6 +21,11 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+        printDeadlineWithStream(tasksData);
+        ArrayList<Task> filteredTasks = filterTaskByString(tasksData, "11");
+        System.out.println("Filtered tasks");
+        printData(filteredTasks);
 
     }
 
@@ -45,5 +51,20 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlineWithStream(ArrayList<Task> tasks) {
+        System.out.println("Print with stream:");
+        tasks.stream().
+                filter((t) -> (t instanceof Deadline)).
+                sorted((t1, t2) -> t1.getDescription().toLowerCase(Locale.ROOT).compareTo(t2.getDescription().toLowerCase(Locale.ROOT))).
+                forEach(System.out::println);
+    }
+
+    public static ArrayList filterTaskByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredTasks = (ArrayList<Task>) tasks.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(Collectors.toList());
+        return filteredTasks;
     }
 }
